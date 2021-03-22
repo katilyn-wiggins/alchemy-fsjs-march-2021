7979111ddf4a7430a4e4ccac42b09e6a2dd782d5
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-03-22 20:15:12 -0300

    cap and filter test working with for loop

--- 01_processes/01_red-green-refactor/lab-01/capitalize-and-filter.js
+++ 01_processes/01_red-green-refactor/lab-01/capitalize-and-filter.js
@@ -1,5 +1,11 @@
-const capitalizeAndFilter = () => {
-  return ['anemone', 'gargantuan', 'yellow', 'squirrel'];
+const capitalizeAndFilter = (arr) => {
+  const newArr = [];
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index].charAt(0) !== 'f') {
+      newArr.push(arr[index].toUpperCase());
+    }
+  }
+  return newArr;
 };
 
 module.exports = capitalizeAndFilter;
--- 01_processes/01_red-green-refactor/lab-01/capitalize-and-filter.test.js
+++ 01_processes/01_red-green-refactor/lab-01/capitalize-and-filter.test.js
@@ -1,9 +1,22 @@
 const capitalizeAndFilter = require('./capitalize-and-filter');
 
+const strangeArray = ['anemone', 'flower', 'gargantuan', 'yellow', 'squirrel', 'freebird'];
+
+const helloArray = ['hello', 'world', 'my', 'friends', 'name', 'is', 'frannie', 'burger'];
+
+const helloFiltArray = ['HELLO', 'WORLD', 'MY', 'NAME', 'IS', 'BURGER'];
+
+
 describe('capitalize and filter function', () => {
   it('should capitlize the given strings and filter out any string beginning with the letter f', () => {
-    const capAndFilt = capitalizeAndFilter(); 
+    const capAndFilt = capitalizeAndFilter(strangeArray); 
 
-    expect(capAndFilt).toEqual(['anemone', 'gargantuan', 'yellow', 'squirrel']); 
+    expect(capAndFilt).toEqual(['ANEMONE', 'GARGANTUAN', 'YELLOW', 'SQUIRREL']); 
+  });
+
+  it('should capitlize the given strings and filter out any string beginning with the letter f', () => {
+    const capAndFilt = capitalizeAndFilter(helloArray); 
+
+    expect(capAndFilt).toEqual(helloFiltArray); 
   });
 }); 
