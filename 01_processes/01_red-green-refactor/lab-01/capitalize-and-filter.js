6ea324b69e78e114ffc2afdbdfdb2b33f97f0dc4
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-03-22 20:20:54 -0300

    cap and filt tests working with array methods

--- 01_processes/01_red-green-refactor/lab-01/capitalize-and-filter.js
+++ 01_processes/01_red-green-refactor/lab-01/capitalize-and-filter.js
@@ -1,11 +1,17 @@
 const capitalizeAndFilter = (arr) => {
-  const newArr = [];
-  for (let index = 0; index < arr.length; index++) {
-    if (arr[index].charAt(0) !== 'f') {
-      newArr.push(arr[index].toUpperCase());
-    }
-  }
-  return newArr;
+  const newArr = arr.map(str => str.toUpperCase()); 
+  const filteredArr = newArr.filter(str => str.charAt(0) !== 'F'); 
+  return filteredArr; 
 };
-
 module.exports = capitalizeAndFilter;
+
+
+
+
+// const newArr = [];
+// for (let index = 0; index < arr.length; index++) {
+//   if (arr[index].charAt(0) !== 'f') {
+//     newArr.push(arr[index].toUpperCase());
+//   }
+// }
+// return newArr;
